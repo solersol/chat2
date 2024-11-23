e87f8dca1153b3016bb68992ab3fd165f03fc495
solersol <ttpage1tk@gmail.com>
2024-11-23 09:55:10 +0000

    Fix real-time chat functionality

--- api/socket.js
+++ api/socket.js
@@ -8,13 +8,17 @@ export default function handler(req, res) {
         io = new Server(res.socket.server);
         res.socket.server.io = io;
 
+        // Handle new connections
         io.on('connection', (socket) => {
             console.log('A user connected:', socket.id);
 
+            // Listen for messages from clients
             socket.on('chat message', (data) => {
-                io.emit('chat message', data);
+                console.log('Message received:', data); // Debug
+                io.emit('chat message', data); // Broadcast to all clients
             });
 
+            // Handle user disconnection
             socket.on('disconnect', () => {
                 console.log('User disconnected:', socket.id);
             });
